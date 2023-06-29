c2fae64f9b5e93e7b5626998b261287ec7b1ce5f
KawshithaFdo <praveenkawshika456@gmail.com>
2023-06-29 14:06:24 +0530

    connect model and predictions to api

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from helper import preprocessing, vectorizer, get_prediction
+
+app = Flask(__name__)
+
+data = dict()
+reviews = []
+positive = 0
+negative = 0
+
+@app.route('/api', methods=['GET'])
+def api():
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+    return data
+
+@app.route('/send-comments/<comment>',methods=['POST'])
+def sendComment(comment):
+    preProcessedText = preprocessing(comment)
+    vectorizedText = vectorizer(preProcessedText)
+    prediction = get_prediction(vectorizedText)
+   
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+
+    res={
+        'prediction': prediction,
+        'negative':negative,
+        'positive':positive
+    }
+    reviews.insert(0, comment)
+    return jsonify(res)
+
+
+if __name__ == '__main__':
+    app.run()
